54894eb09c0a766d91f37e9e34bc75e5244c4b1f
Descalzo404 <63608057+Descalzo404@users.noreply.github.com>
2020-09-10 20:30:11 -0300

    Added the option to go up and down

--- game/ship.py
+++ game/ship.py
@@ -20,21 +20,29 @@ class Ship():
         self.rect.bottom = self.screen_rect.bottom
 
         #Keeps a float value for the center of the spaceship
-        self.center = float(self.rect.centerx)
+        self.center_x = float(self.rect.centerx)
+        self.center_y = float(self.rect.bottom)
 
         #Moviment's flags
         self.moving_right = False
         self.moving_left = False
+        self.moving_up = False
+        self.moving_down = False
 
     def update(self):
         """Updates the position of the spaceship"""
         if self.moving_right:
-            self.center += self.ai_settings.ship_speed_factor
+            self.center_x += self.ai_settings.ship_speed_factor
         if self.moving_left:
-            self.center -= self.ai_settings.ship_speed_factor
+            self.center_x -= self.ai_settings.ship_speed_factor
+        if self.moving_up:
+            self.center_y -= self.ai_settings.ship_speed_factor
+        if self.moving_down:
+            self.center_y += self.ai_settings.ship_speed_factor
 
         #updates the object rect according to self.center
-        self.rect.centerx = self.center
+        self.rect.centerx = self.center_x
+        self.rect.bottom = self.center_y
 
     def blitme(self):
         """Draw the spaceship in its actual position"""

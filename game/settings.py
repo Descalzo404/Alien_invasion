ed0a32f4b8b846e0d39023d159048074e6917c3a
Descalzo404 <63608057+Descalzo404@users.noreply.github.com>
2020-09-09 23:20:18 -0300

    Created the game settings

---
+++ game/settings.py
@@ -0,0 +1,9 @@
+class Settings():
+    #A class to keep all settings from the game
+
+    def __init__(self):
+        """Start the game's settings"""
+        #Screen settings
+        self.screen_width = 1200
+        self.screnn_height = 800
+        self.bg_color = (68,71,90)

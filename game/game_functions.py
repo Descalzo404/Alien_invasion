ac2da787c5cf3cfc20fe820048353c73ad75ae41
Descalzo404 <63608057+Descalzo404@users.noreply.github.com>
2020-09-11 20:30:51 -0300

    reorganized bullet functions

--- game/game_functions.py
+++ game/game_functions.py
@@ -9,10 +9,8 @@ def check_keydown_events(event, ai_settings, screen, ship, bullets):
     elif event.key == pygame.K_LEFT:
         ship.moving_left = True
     elif event.key == pygame.K_SPACE:
-        #Creates a new bullet and adds it to the group of bullets
-        if len(bullets) < ai_settings.bullets_allowed:
-            new_bullet = Bullet(ai_settings, screen, ship)
-            bullets.add(new_bullet)
+        fire_bullet(ai_settings, screen, ship, bullets)
+        
 
 def check_keyup_events(event, ship):
     """Answer to when the key is released"""
@@ -51,4 +49,11 @@ def update_bullets(bullets):
     for bullet in bullets.copy():
         if bullet.rect.bottom <= 0:
             bullets.remove(bullet)
+
+def fire_bullet(ai_settings, screen, ship, bullets):
+    """Fires a bullet if the limit isnt surpassed"""
+    #Creates a new bullet and add it to the group of bullets
+    if len(bullets) < ai_settings.bullets_allowed:
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
     

c7df4afaaa6844b2c92c7daf042fd9358d4e0e50
Descalzo404 <63608057+Descalzo404@users.noreply.github.com>
2020-09-10 21:25:38 -0300

    refacture of the check_events() function

--- game/game_functions.py
+++ game/game_functions.py
@@ -1,23 +1,29 @@
 import sys
 import pygame
 
+def check_keydown_events(event, ship):
+    """Answer to when the key is pressed"""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+
+def check_keyup_events(event, ship):
+    """Answer to when the key is released"""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+         ship.moving_left = False
+
 def check_events(ship):
     """Answer for events in the keyboard or mouse"""
     for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    ship.moving_right = True
-                elif event.key == pygame.K_LEFT:
-                    ship.moving_left = True
-
+                check_keydown_events(event, ship)
             elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_RIGHT:
-                    ship.moving_right = False
-                elif event.key == pygame.K_LEFT:
-                    ship.moving_left = False
+                check_keyup_events(event, ship)
 
 def update_screen(ai_settings, screen, ship):
     """Updates the screen"""
